e67c8abf740a728c3e1e14231f3073c754c364ea
StefanSun <821041976@qq.com>
2018-09-20 15:30:58 +0800

    1

---
+++ Assets/GetSceneFromFTP.cs
@@ -0,0 +1,279 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+// 添加命令空间
+using System.Net;
+using System.Text;
+using System.Threading;
+
+
+
+public class GetSceneFromFTP : MonoBehaviour
+{
+
+    public Thread threadLoadImage;
+    private const int ftpport = 21;
+    private string ftpUristring = null;
+    private NetworkCredential networkCredential;
+    public string ftppath;
+    private string currentDir = "/";
+    public string downname;
+    public GameObject LoginInformation;//显示是否登录成功
+    public GameObject SceneListMenu;
+    void Start()
+    {
+
+    }
+    #region 与服务器的交互
+    //创建FTP连接
+    public FtpWebRequest CreateFtpWebRequest(string uri, string requestMethod)
+    {
+        FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(uri);
+        request.Credentials = networkCredential;
+        request.KeepAlive = true;
+        request.UseBinary = true;
+        request.Method = requestMethod;
+        return request;
+    }
+    // 获取服务器返回的响应体
+    public FtpWebResponse GetFtpResponse(FtpWebRequest request)
+    {
+        FtpWebResponse response = null;
+        try
+        {
+            response = (FtpWebResponse)request.GetResponse();
+            return response;
+        }
+        catch (WebException ex)
+        {
+            return null;
+        }
+    }
+    #endregion
+    // 登录服务器事件
+    public void btnlogin_Click()
+    {
+        ftpUristring = "ftp://" + ftppath;
+        networkCredential = new NetworkCredential("Administrator", "Qwe123456789.");
+        if (ShowFtpFileAndDirectory() == true)
+        {
+            Debug.Log("连接成功");
+            SceneListMenu.SetActive(true);
+        }
+        else
+        {
+            LoginInformation.gameObject.SetActive(true);
+            Text infor = GameObject.Find("Infor").GetComponent<Text>();
+            infor.text = "连接失败，请检查网络";
+        }
+    }
+
+
+    // 显示资源列表
+    public bool ShowFtpFileAndDirectory()
+    {
+        try
+        {
+            string uri = string.Empty;
+            if (currentDir == "/")
+            {
+                uri = ftpUristring;
+            }
+            else
+            {
+                uri = ftpUristring + currentDir;
+            }
+            uri = "ftp://" + ftppath;
+            FtpWebRequest request = CreateFtpWebRequest(uri, WebRequestMethods.Ftp.ListDirectoryDetails);
+            // 获得服务器返回的响应信息
+            FtpWebResponse response = GetFtpResponse(request);
+            Debug.Log(response);
+            if (response == null)
+            {
+                return false;
+            }
+            // 读取网络流数据
+            Stream stream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream, Encoding.Default);
+            string s = streamReader.ReadToEnd();
+            streamReader.Close();
+            stream.Close();
+            response.Close();
+            // 处理并显示文件目录列表
+            string[] ftpdir = s.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            int length = 0;
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                if (ftpdir[i].EndsWith("."))
+                {
+                    length = ftpdir[i].Length - 2;
+                    break;
+                }
+            }
+
+
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                s = ftpdir[i];
+                int index = s.LastIndexOf('\t');
+                if (index == -1)
+                {
+                    if (length < s.Length)
+                    {
+                        index = length;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+
+                string name = s.Substring(index + 1);
+                if (name == "." || name == "..")
+                {
+                    continue;
+                }
+                // 判断是否为目录，在名称前加"目录"来表示
+                if (s[0] == 'd' || (s.ToLower()).Contains("<dir>"))
+                {
+                    string[] namefield = name.Split(' ');
+                    int namefieldlength = namefield.Length;
+                    string dirname;
+                    dirname = namefield[namefieldlength - 1];
+                    dirname = dirname.PadRight(34, ' ');
+                    name = dirname;
+
+
+                }
+            }
+            for (int i = 0; i < ftpdir.Length; i++)
+            {
+                s = ftpdir[i];
+                int index = s.LastIndexOf('\t');
+                if (index == -1)
+                {
+                    if (length < s.Length)
+                    {
+                        index = length;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                string name = s.Substring(index + 1);
+                if (name == "." || name == "..")
+                {
+                    continue;
+                }
+                // 判断是否为文件
+                if (!(s[0] == 'd' || (s.ToLower()).Contains("<dir>")))
+                {
+                    string[] namefield = name.Split(' ');
+                    int namefieldlength = namefield.Length;
+                    string filename;
+                    filename = namefield[namefieldlength - 1];
+                    // 对齐
+                    filename = filename.PadRight(34, ' ');
+                    name = filename;
+                    // 显示文件
+                }
+                Debug.Log(name);
+            }
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    #region
+    public string GetUriString(string filename)
+    {
+        string uri = string.Empty;
+        if (currentDir.EndsWith("/"))
+        {
+            uri = ftpUristring + currentDir + filename;
+        }
+        else
+        {
+            uri = ftpUristring + currentDir + "/" + filename;
+        }
+        return uri;
+    }
+    // 从服务器上下载文件到本地事件
+    public void btndownload_Click()
+    {
+        string s = UnityEngine.Random.Range(1, 99).ToString();
+        threadLoadImage = new Thread(LoadImage);
+        threadLoadImage.Start(s);
+    }
+    // 获得选择的文件
+
+    // 如果选择的是目录或者是返回上层目录，则返回null
+
+    //该函数要挂在Button上
+
+    public string GetSelectedFile()
+    {
+        string filename = downname;
+        return filename;
+    }
+    // 删除服务器文件事件
+    #endregion
+
+    void LoadImage(object str)
+    {
+
+        string fileName = GetSelectedFile();
+        if (fileName.Length == 0)
+        {
+            threadLoadImage.Abort();
+        }
+        // 选择保存文件的位置
+
+        string filePath = "D://" + str.ToString() + fileName;//要具体到名字
+        try
+        {
+            string uri = GetUriString(fileName);
+            FtpWebRequest request = CreateFtpWebRequest(uri, WebRequestMethods.Ftp.DownloadFile);
+            FtpWebResponse response = GetFtpResponse(request);
+            if (response == null)
+            {
+                threadLoadImage.Abort();
+            }
+            Stream responseStream = response.GetResponseStream();
+            FileStream filestream = File.Create(filePath);
+            int buflength = 8196;
+            byte[] buffer = new byte[buflength];
+            int bytesRead = 1;
+            while (bytesRead != 0)
+            {
+                bytesRead = responseStream.Read(buffer, 0, buflength);
+                filestream.Write(buffer, 0, bytesRead);
+            }
+            //LoginInformation.gameObject.SetActive(true);
+            //Text infor = GameObject.Find("Infor").GetComponent<Text>();
+            //infor.text = "下载成功";
+            //SceneListMenu.SetActive(false);
+            responseStream.Close();
+            filestream.Close();
+
+            threadLoadImage.Abort();
+        }
+        catch (WebException ex)
+        {
+            //LoginInformation.gameObject.SetActive(true);
+            //Text infor = GameObject.Find("Infor").GetComponent<Text>();
+            //infor.text = "下载失败";
+            threadLoadImage.Abort();
+        }
+    }
+}
+
+
+
---
+++ Assets/UploadToFTP.cs
@@ -0,0 +1,148 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Globalization;
+using System.Text;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UploadToFTP : MonoBehaviour
+{
+
+    public string _localFile;
+    public string _targerGir;
+    public string _hostName;
+    public string _userName;
+    public string _password;
+
+    /// <summary>
+    /// 上传文件
+    /// </summary>
+    /// <param name="fileinfo">需要上传的文件</param>
+    /// <param name="targetDir">目标路径</param>
+    /// <param name="hostname">ftp地址</param>
+    /// <param name="username">ftp用户名</param>
+    /// <param name="password">ftp密码</param>
+    public static void UploadFile(FileInfo fileinfo, string targetDir, string hostname, string username, string password)
+    {
+        //1. check target
+        string target;
+        if (targetDir.Trim() == "")
+        {
+            return;
+        }
+        target = Guid.NewGuid().ToString();  //使用临时文件名
+
+
+        string URI = "FTP://" + hostname + "/" + targetDir + "/" + target;
+        ///WebClient webcl = new WebClient();
+        System.Net.FtpWebRequest ftp = GetRequest(URI, username, password);
+
+        //设置FTP命令 设置所要执行的FTP命令，
+        //ftp.Method = System.Net.WebRequestMethods.Ftp.ListDirectoryDetails;//假设此处为显示指定路径下的文件列表
+        ftp.Method = System.Net.WebRequestMethods.Ftp.UploadFile;
+        //指定文件传输的数据类型
+        ftp.UseBinary = true;
+        ftp.UsePassive = true;
+
+        //告诉ftp文件大小
+        ftp.ContentLength = fileinfo.Length;
+        //缓冲大小设置为2KB
+        const int BufferSize = 2048;
+        byte[] content = new byte[BufferSize - 1 + 1];
+        int dataRead;
+
+        Text infor = GameObject.Find("Infor").GetComponent<Text>();
+        infor.text = "开始上传。。。";
+        //打开一个文件流 (System.IO.FileStream) 去读上传的文件
+        using (FileStream fs = fileinfo.OpenRead())
+        {
+            try
+            {
+                //把上传的文件写入流
+                using (Stream rs = ftp.GetRequestStream())
+                {
+                    do
+                    {
+                        //每次读文件流的2KB
+                        dataRead = fs.Read(content, 0, BufferSize);
+                        rs.Write(content, 0, dataRead);
+                    } while (!(dataRead < BufferSize));
+                    rs.Close();
+                }
+
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                fs.Close();
+            }
+
+        }
+        ftp = null;
+        //设置FTP命令
+        ftp = GetRequest(URI, username, password);
+        ftp.Method = System.Net.WebRequestMethods.Ftp.Rename; //改名
+        ftp.RenameTo = UnityEngine.Random.Range(1, 100).ToString() + fileinfo.Name;
+        infor.text = ftp.RenameTo + ">>>上传完毕。。。";
+        try
+        {
+            ftp.GetResponse();
+        }
+        catch (Exception ex)
+        {
+            ftp = GetRequest(URI, username, password);
+            ftp.Method = System.Net.WebRequestMethods.Ftp.DeleteFile; //删除
+            ftp.GetResponse();
+            throw ex;
+        }
+        finally
+        {
+            //fileinfo.Delete();
+        }
+
+        // 可以记录一个日志  "上传" + fileinfo.FullName + "上传到" + "FTP://" + hostname + "/" + targetDir + "/" + fileinfo.Name + "成功." );
+        ftp = null;
+
+        #region
+        /*****
+         *FtpWebResponse
+         * ****/
+        //FtpWebResponse ftpWebResponse = (FtpWebResponse)ftp.GetResponse();
+        #endregion
+    }
+
+    private static FtpWebRequest GetRequest(string URI, string username, string password)
+    {
+        //根据服务器信息FtpWebRequest创建类的对象
+        FtpWebRequest result = (FtpWebRequest)FtpWebRequest.Create(URI);
+        //提供身份验证信息
+        result.Credentials = new System.Net.NetworkCredential(username, password);
+        //设置请求完成之后是否保持到FTP服务器的控制连接，默认值为true
+        result.KeepAlive = false;
+        return result;
+    }
+
+    public void UploadImage()
+    {
+        FileInfo info = new FileInfo(_localFile);
+
+        UploadFile(info, _targerGir, _hostName, _userName, _password);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
